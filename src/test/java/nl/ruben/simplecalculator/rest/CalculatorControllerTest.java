a26f5e6088c21dcd78ee99a66725f99f0f3f5079
rsmit <rsmit@quintor.local>
2020-12-18 09:51:54 +0100

    Added more controller tests

--- src/test/java/nl/ruben/simplecalculator/rest/CalculatorControllerTest.java
+++ src/test/java/nl/ruben/simplecalculator/rest/CalculatorControllerTest.java
@@ -15,19 +15,17 @@ import javax.validation.constraints.NotNull;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class CalculatorControllerTest {
 
     private CalculatorController calculatorController;
+    private CalculatorService calculatorService;
     @BeforeEach
     void setUp(@Mock CalculatorService calculatorService) {
         calculatorController = new CalculatorController(calculatorService);
-        Mockito.lenient()
-                .when(calculatorService.add(any(CalculationDto.class)))
-                    .thenReturn(9999.9999);
-
-
+        this.calculatorService = calculatorService;
     }
 
     @AfterEach
@@ -36,22 +34,59 @@ class CalculatorControllerTest {
 
     @Test
     void add() {
+        Mockito.lenient()
+                .when(calculatorService.add(any(CalculationDto.class)))
+                .thenReturn(9999.9999);
+
         CalculationDto dto = new CalculationDto();
         dto.setLeft(999);
         dto.setRight(4);
         ResponseEntity<Double> result = calculatorController.add(dto);
+        verify(calculatorService).add(dto);
+        assertTrue(result.getStatusCode().is2xxSuccessful());
         assertEquals(9999.9999, result.getBody());
     }
 
     @Test
     void subtract() {
+        Mockito.lenient()
+                .when(calculatorService.subtract(any(CalculationDto.class)))
+                .thenReturn(9999.9999);
+        CalculationDto dto = new CalculationDto();
+        dto.setLeft(999);
+        dto.setRight(4);
+        ResponseEntity<Double> result = calculatorController.subtract(dto);
+        verify(calculatorService).subtract(dto);
+        assertTrue(result.getStatusCode().is2xxSuccessful());
+        assertEquals(9999.9999, result.getBody());
     }
 
     @Test
     void multiply() {
+        Mockito.lenient()
+                .when(calculatorService.multiply(any(CalculationDto.class)))
+                .thenReturn(9999.9999);
+        CalculationDto dto = new CalculationDto();
+        dto.setLeft(999);
+        dto.setRight(4);
+        ResponseEntity<Double> result = calculatorController.multiply(dto);
+        verify(calculatorService).multiply(dto);
+        assertTrue(result.getStatusCode().is2xxSuccessful());
+        assertEquals(9999.9999, result.getBody());
     }
 
     @Test
     void divide() {
+        Mockito.lenient()
+                .when(calculatorService.divide(any(CalculationDto.class)))
+                .thenReturn(9999.9999);
+
+        CalculationDto dto = new CalculationDto();
+        dto.setLeft(999);
+        dto.setRight(4);
+        ResponseEntity<Double> result = calculatorController.divide(dto);
+        verify(calculatorService).divide(dto);
+        assertTrue(result.getStatusCode().is2xxSuccessful());
+        assertEquals(9999.9999, result.getBody());
     }
 }
